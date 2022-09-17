1bf7aba26dbcb5493156ea6519c1cab9379bfbe0
Azkiya <88168463+EmiliaTzy@users.noreply.github.com>
2022-09-17 12:43:09 +0200

    Add files via upload

---
+++ Tianabot/modules/zombie.py
@@ -0,0 +1,105 @@
+import asyncio
+
+from asyncio import sleep
+from telethon import events
+from telethon.errors import ChatAdminRequiredError, UserAdminInvalidError
+from telethon.tl.functions.channels import EditBannedRequest
+from telethon.tl.types import ChatBannedRights, ChannelParticipantsAdmins
+
+from Tianabot import telethn, OWNER_ID, DEV_USERS, DRAGONS, DEMONS
+
+# =================== CONSTANT ===================
+
+BANNED_RIGHTS = ChatBannedRights(
+    until_date=None,
+    view_messages=True,
+    send_messages=True,
+    send_media=True,
+    send_stickers=True,
+    send_gifs=True,
+    send_games=True,
+    send_inline=True,
+    embed_links=True,
+)
+
+
+UNBAN_RIGHTS = ChatBannedRights(
+    until_date=None,
+    send_messages=None,
+    send_media=None,
+    send_stickers=None,
+    send_gifs=None,
+    send_games=None,
+    send_inline=None,
+    embed_links=None,
+)
+
+OFFICERS = [OWNER_ID] + DEV_USERS + DRAGONS + DEMONS
+
+# Check if user has admin rights
+
+async def is_administrator(user_id: int, message):
+    admin = False
+    async for user in telethn.iter_participants(
+        message.chat_id, filter=ChannelParticipantsAdmins
+    ):
+        if user_id == user.id or user_id in OFFICERS:
+            admin = True
+            break
+    return admin
+
+
+@telethn.on(events.NewMessage(pattern="^[!/]zombies ?(.*)"))
+async def rm_deletedacc(show):
+    con = show.pattern_match.group(1).lower()
+    del_u = 0
+    del_status = "**Grup bersih, 0 akun terhapus ditemukan.**"
+    if con != "clean":
+        kontol = await show.reply("`Mencari akun terhapus untuk ditendang...`")
+        async for user in show.client.iter_participants(show.chat_id):
+            if user.deleted:
+                del_u += 1
+                await sleep(1)
+        if del_u > 0:
+            del_status = (
+                f"**Mencari...** `{del_u}` **Akun Terhapus/Zombie Di Grup Ini,"
+                "\nHapus dengan perintah** `/zombies clean`"
+            )
+        return await kontol.edit(del_status)
+    chat = await show.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+    if not admin and not creator:
+        return await show.reply("**Maaf Anda bukan admin😔**")
+    memek = await show.reply("`Menendang akun terhapus...`")
+    del_u = 0
+    del_a = 0
+    async for user in telethn.iter_participants(show.chat_id):
+        if user.deleted:
+            try:
+                await show.client(
+                    EditBannedRequest(show.chat_id, user.id, BANNED_RIGHTS)
+                )
+            except ChatAdminRequiredError:
+                return await show.edit("`Tidak Memiliki Hak Blokir Pengguna Di Grup Ini`")
+            except UserAdminInvalidError:
+                del_u -= 1
+                del_a += 1
+            await telethn(EditBannedRequest(show.chat_id, user.id, UNBAN_RIGHTS))
+            del_u += 1
+    if del_u > 0:
+        del_status = f"**Membersihkan** `{del_u}` **Zombie**"
+    if del_a > 0:
+        del_status = (
+            f"**Cleaned** `{del_u}` **Zombie** "
+            f"\n`{del_a}` **Admin akun terhapus tidak dapat ditendang.**"
+        )
+    await memek.edit(del_status)
+
+__help__ = """
+*Menghapus Akun Terhapus*
+ ❍ /zombies *:* Mulai mencari akun terhapus dari grup.
+ ❍ /zombies clean *:* Menghapus akun terhapus dari grup.
+"""
+
+__mod_name__ = "𝙰𝚔𝚞𝚗 𝙼𝚊𝚝𝚒 👻"
