9891770b7ecddcf99a015eecb3e77e2026c479aa
Azkiya <88168463+EmiliaTzy@users.noreply.github.com>
2022-10-02 14:01:43 +0700

    Update debug.py

--- Tianabot/modules/debug.py
+++ Tianabot/modules/debug.py
@@ -21,15 +21,15 @@ def debug(update: Update, context: CallbackContext):
     if len(args) > 1:
         if args[1] in ("yes", "on"):
             DEBUG_MODE = True
-            message.reply_text("Debug mode is now on.")
+            message.reply_text("Mode Debug telah aktif.")
         elif args[1] in ("no", "off"):
             DEBUG_MODE = False
-            message.reply_text("Debug mode is now off.")
+            message.reply_text("Mode Debug telah nonaktif.")
     else:
         if DEBUG_MODE:
-            message.reply_text("Debug mode is currently on.")
+            message.reply_text("Mode Debug sekarang aktif.")
         else:
-            message.reply_text("Debug mode is currently off.")
+            message.reply_text("Mode Debug sekarang nonaktifk.")
 
 
 @telethn.on(events.NewMessage(pattern="[/!].*"))
