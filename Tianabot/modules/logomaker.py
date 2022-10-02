0b1366d4131e116fc12c5b5b26f065fe1f1d7b9b
Azkiya <88168463+EmiliaTzy@users.noreply.github.com>
2022-10-02 13:56:31 +0700

    Update logomaker.py

--- Tianabot/modules/logomaker.py
+++ Tianabot/modules/logomaker.py
@@ -254,9 +254,9 @@ LOGO_LINKS            = ["https://telegra.ph/file/d1838efdafce9fe611d0c.jpg",
 async def lego(event):
  quew = event.pattern_match.group(1)
  if event.sender_id != OWNER_ID and not quew:
-  await event.reply('`Please give me the text for the logo!`\n`Example /logo <pantek>`')
+  await event.reply('`Tolong beri saya teks untuk logo!`\n`Contoh /logo <keren>`')
   return
- pesan = await event.reply('`Creating your logo...`')
+ pesan = await event.reply('Membuat Logomu...`')
  try:
     text = event.pattern_match.group(1)
     randc = random.choice(LOGO_LINKS)
@@ -276,16 +276,16 @@ async def lego(event):
     x = (image_widthz-w)/2
     y = ((image_heightz-h)/2+6)
     draw.text((x, y), text, font=font, fill="white", stroke_width=1, stroke_fill="black")
-    fname = "Tiana.png"
+    fname = "Eiko.png"
     img.save(fname, "png")
-    await telethn.send_file(event.chat_id, file=fname, caption = f"Made by [Tiana](@Tiana_prince_Bot)")         
+    await telethn.send_file(event.chat_id, file=fname, caption = f"Dibuat Oleh [Eiko](@EikoManager_Bot)")         
     await pesan.delete()
     if os.path.exists(fname):
             os.remove(fname)
  except Exception as e:
-    await event.reply(f'Error, Report @{SUPPORT_GROUP}')
+    await event.reply(f'Error, Laporkan @{SUPPORT_GROUP}')
 
 __help__ = """
- - /logo <text> :  Create your logo with your name
+ - /logo <teks> :  Buat logo Anda dengan nama Anda
  """
-__mod_name__ = "Lᴏɢᴏ"
+__mod_name__ = "Logo 🎨"
