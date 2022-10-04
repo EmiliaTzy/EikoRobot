3155bf99a0a2301b2099312f22c8ffe5da3eac3c
Azkiya <88168463+EmiliaTzy@users.noreply.github.com>
2022-10-04 15:26:41 +0700

    Update dev.py

--- Tianabot/modules/dev.py
+++ Tianabot/modules/dev.py
@@ -18,14 +18,14 @@ from telegram.ext import CallbackContext, CommandHandler, run_async
 def allow_groups(update: Update, context: CallbackContext):
     args = context.args
     if not args:
-        update.effective_message.reply_text(f"Current state: {Tianabot.ALLOW_CHATS}")
+        update.effective_message.reply_text(f"Kondisi saat ini: {Tianabot.ALLOW_CHATS}")
         return
     if args[0].lower() in ["off", "no"]:
         Tianabot.ALLOW_CHATS = True
     elif args[0].lower() in ["yes", "on"]:
         Tianabot.ALLOW_CHATS = False
     else:
-        update.effective_message.reply_text("Format: /lockdown Yes/No or Off/On")
+        update.effective_message.reply_text("Format: /lockdown Yes/No atau Off/On")
         return
     update.effective_message.reply_text("Done! Lockdown value toggled.")
 
@@ -40,30 +40,30 @@ def leave(update: Update, context: CallbackContext):
             bot.leave_chat(int(chat_id))
         except TelegramError:
             update.effective_message.reply_text(
-                "Beep boop, I could not leave that group(dunno why tho)."
+                "Beep boop, Saya tidak bisa meninggalkan grup itu (tidak tahu mengapa)."
             )
             return
         with suppress(Unauthorized):
-            update.effective_message.reply_text("Beep boop, I left that soup!.")
+            update.effective_message.reply_text("Beep boop, Saya meninggalkan grup itu!.")
     else:
-        update.effective_message.reply_text("Send a valid chat ID")
+        update.effective_message.reply_text("Kirim ID obrolan yang valid")
 
 
 @run_async
 @dev_plus
 def gitpull(update: Update, context: CallbackContext):
     sent_msg = update.effective_message.reply_text(
-        "Pulling all changes from remote and then attempting to restart."
+        "Menarik semua perubahan dari jarak jauh dan kemudian mencoba memulai kembali."
     )
     subprocess.Popen("git pull", stdout=subprocess.PIPE, shell=True)
 
-    sent_msg_text = sent_msg.text + "\n\nChanges pulled...I guess.. Restarting in "
+    sent_msg_text = sent_msg.text + "\n\nPerubahan ditarik ... Saya kira .. Mulai ulang dalam"
 
     for i in reversed(range(5)):
         sent_msg.edit_text(sent_msg_text + str(i + 1))
         sleep(1)
 
-    sent_msg.edit_text("Restarted.")
+    sent_msg.edit_text("Memulai Ulang.")
 
     os.system("restart.bat")
     os.execv("start.bat", sys.argv)
@@ -73,7 +73,7 @@ def gitpull(update: Update, context: CallbackContext):
 @dev_plus
 def restart(update: Update, context: CallbackContext):
     update.effective_message.reply_text(
-        "Starting a new instance and shutting down this one"
+        "Memulai instance baru dan mematikan yang ini"
     )
 
     os.system("restart.bat")
@@ -90,5 +90,5 @@ dispatcher.add_handler(LEAVE_HANDLER)
 dispatcher.add_handler(GITPULL_HANDLER)
 dispatcher.add_handler(RESTART_HANDLER)
 
-__mod_name__ = "ᴇᴠs"
+__mod_name__ = "Evs"
 __handlers__ = [LEAVE_HANDLER, GITPULL_HANDLER, RESTART_HANDLER, ALLOWGROUPS_HANDLER]
