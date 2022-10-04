c18593f94bd206a208c63044f5fc4cf3da1ac4ef
Azkiya <88168463+EmiliaTzy@users.noreply.github.com>
2022-10-04 15:29:04 +0700

    Update disable.py

--- Tianabot/modules/disable.py
+++ Tianabot/modules/disable.py
@@ -348,7 +348,7 @@ if is_module_loaded(FILENAME):
     ❍ /listcmds*:* list all possible toggleable commands
     """
 
-    __mod_name__ = "Dɪsᴀʙʟᴇ"
+    __mod_name__ = "Mematikan ❌"
 
 else:
     DisableAbleCommandHandler = CommandHandler
